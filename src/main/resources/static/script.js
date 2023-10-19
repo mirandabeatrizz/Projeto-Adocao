b08874ff1312cc5b83df38254295ff37fc3c21f3
ZyoncoreSistemas <mirandabeatrizaparecida@gmail.com>
2023-10-19 10:43:22 -0300

    ajustes

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,18 +1,82 @@
+
+//LISTAR ANIMAIS CADASTRADOS
+function listarAnimais(){
+    fetch('/animais/listar')
+    .then(response => response.json())
+    .then(data =>{
+        const listaAnimais = document.getElementById('listaAnimais');
+        listaAnimais.innerHTML = '';
+        
+        data.forEach(animal => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = ` <td>${animal.nome}</td>
+            <td>${animal.tipo}</td>
+            <td>${animal.porte}</td>
+            <td>${animal.idade}</td>
+            <td>${animal.foto}</td>
+            <td>${animal.castrado}</td>
+            <td>${animal.vacinado}</td>
+            <td> 
+                <button class="botao-excluir flat-button" data-id="${animal.id}">Excluir</button>
+            </td> `;
+
+            listaAnimais.appendChild(tr);
+        });
+        //BOTÃO PRA CHAMAR O EXCLUIR
+        const botoesExcluir = document.querySelectorAll('.botao-excluir');
+        const botoesEditar = document.querySelectorAll('.botao-editar');
+
+     
+
+        botoesExcluir.forEach(botao => {
+            botao.addEventListener('click', function () {
+                const id = botao.getAttribute('data-id');
+                // Confirmar com o usuário antes de excluir
+                const confirmacao = confirm('Tem certeza de que deseja excluir este animal?');
+               
+
+                if (confirmacao) {
+                    // Enviar a solicitação DELETE
+                    fetch(`/animais/excluir/${id}`, {
+                        method: 'DELETE'
+                    })
+                        .then(response => {
+                            if (response.ok) {
+                                window.confirm(`Animal com ID ${id} excluído com sucesso.`);
+                                listarAnimais();
+                            } else {
+                                console.error(`Erro ao excluir o animal com ID ${id}`);
+                            }
+                        })
+                        .catch(error => {
+                            console.error(`Erro ao excluir o animal com ID ${id}:`, error);
+                        });
+                }
+            });
+        });
+
+    })
+    .catch(error => {
+        console.error('Erro ao listar produtos:', error);
+    });
+}
+//CADASTRAR NOVO ANIMAL
 document.getElementById('addAnimais').addEventListener('click', function(){
     const nomeInput = document.getElementById('nome');
     const tipoInput = document.getElementById('tipo');
     const porteInput = document.getElementById('porte');
     const idadeInput = document.getElementById('idade');
     const fotoInput = document.getElementById('foto');
-    const castradorInput = document.getElementById('castrado');
-    const vacinadoInput = document.getElementById('vacinado');
+    const castradoInput = document.querySelector('input[name="castrado"]:checked');
+    const vacinadoInput = document.querySelector('input[name="vacinado"]:checked');
+
 
     const nome = nomeInput.value;
     const tipo = tipoInput.value;
     const porte = porteInput.value;
     const idade = idadeInput.value;
     const foto = fotoInput.value;
-    const castrado = castradorInput.value;
+    const castrado = castradoInput.value;
     const vacinado = vacinadoInput.value;
 
     fetch('/animais/adicionar', {
@@ -38,13 +102,13 @@ document.getElementById('addAnimais').addEventListener('click', function(){
         porteInput.value = '';
         idadeInput.value = '';
         fotoInput.value = '';
-        castradorInput.value = '';
+        castradoInput.value = '';
         vacinadoInput.value = '';
     })
     .catch(error => {
-        console.error('Erro ao adicionar livro:', error);
+        console.error('Erro ao adicionar animal:', error);
     });
 
-
-
 });
+
+listarAnimais();
