acf64cf5826d29f863a4804500e7ddf929b47afa
ZyoncoreSistemas <mirandabeatrizaparecida@gmail.com>
2023-10-24 15:48:44 -0300

    .

--- src/main/java/com/projetoadocao/controller/AuthController.java
+++ src/main/java/com/projetoadocao/controller/AuthController.java
@@ -1,7 +1,11 @@
 package com.projetoadocao.controller;
 
+import javax.naming.Binding;
+
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projetoadocao.dto.AdmDto;
+import com.projetoadocao.entities.Administradores;
 import com.projetoadocao.service.AdminService;
 
 @RestController
@@ -10,6 +14,29 @@ public class AuthController {
     private AdminService adminService;
     public AuthController(AdminService adminService){
         this.adminService = adminService;
+    }
+    @GetMapping("/cadastro")
+    public String FormularioCadastro(Model model){
+        AdmDto adm = new AdmDto();
+        model.addAttribute("adm", adm);
+    }
+    @PostMapping("/addCad")
+    public String cadastro(@Valid @ModelAttribute("adm") AdmDto adm,
+                            BindingResult result,
+                            Model model){
+            Administradores existing = adminService.findByEmail(adm.getEmail());
+            if (existing != null) {
+                result.rejectValue("email", null, "Ja existe alguem com este email");
+            }
+            if (result.hasErros()){
+                model.addAttribute("adm", adm);
+            }
+            adminService.saveAdm(adm);
+            }
+    @GetMapping("/adms")
+    public String listaAdms(Model model){
+        List<AdmDto> adms = adminService.findAllAdms();
+        model.addAttribute("adms", adms);
     }
     
 }
