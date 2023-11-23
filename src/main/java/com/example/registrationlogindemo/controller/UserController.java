3dce25d95608cc8edb9814217c7b02e2650f4e34
ZyoncoreSistemas <mirandabeatrizaparecida@gmail.com>
2023-11-23 16:16:44 -0300

    teste

---
+++ src/main/java/com/example/registrationlogindemo/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.example.registrationlogindemo.controller;
+
+import java.io.File;
+import java.util.List;
+import java.util.Optional;
+
+import com.example.registrationlogindemo.entity.User;
+import com.example.registrationlogindemo.repository.UserRepository;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.persistence.EntityNotFoundException;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserController (UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/listar")
+    public List <User> listarUsuarios(){
+        return UserRepository.findAll();
+    }
+    
+    @PutMapping("/editar/{id}")
+    public User editaUser(@PathVariable Long id, @RequestBody User novoUser){
+        return userRepository.findById(id).map(user ->{
+            user.setName(novoUser.getName());
+            user.setEmail(novoUser.getEmail());
+            user.setPassword(novoUser.getPassword());
+            return userRepository.save(user);
+        })
+        .orElseGet(() ->{
+            throw new EntityNotFoundException();
+        });
+    }
+
+    @DeleteMapping("/excluir/{id}")
+    public void excluirUser(@PathVariable Long id){
+        userRepository.deleteById(id);
+    }
+}
--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -1,37 +1,5 @@
-document.getElementById('fzrLogin').addEventListener('click', function(){
-    const nomeInput = document.getElementById('nome');
-    const emailInput = document.getElementById('email');
-    const senhaInput = document.getElementById('senha');
-
-    const nome = nomeInput.value;
-    const email = emailInput.value;
-    const senha = senhaInput.value;
-
-    fetch('/adm/adicionar',{
-        method: 'POST',
-        headers:{
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify({
-            nome: nome,
-            email: email,
-            senha: senha
-        })
-    })
-    .then(response => response.json())
-    .then(data =>{
-        window.confirm(`Usuário ${data.nome} adicionado com sucesso`);
-        nomeInput.value ='';
-        emailInput.value ='';
-        senhaInput.value = '';
-    })
-    .catch(error=>{
-        console.error('erro ao adicionar usuário:', error);
-    })
-});
-
 function listarUsers(){
-    fetch('/adm/listar')
+    fetch('/user/listar')
     .then(response => response.json())
     .then (data =>{
         const listaUsers = document.getElementById('listaUsers');
@@ -40,11 +8,11 @@ function listarUsers(){
         data.forEach(user =>{
             const tr = document.createElement('tr');
             tr.innerHTML = ` 
-            <td> ${user.id}</td>
             <td> ${user.nome}</td>
             <td> ${user.email}</td>
-            <td> ${user.senha}</td>
-            <td> <button class="btn-excluir" data-id="${user.id}">  Excluir </buttom>
+            <td> 
+            <button class="btn-excluir" data-id="${user.id}">  Excluir </buttom>
+            <button class="btn-editar" data-id="${user.id}">  Editar </buttom>
             </td>`;
             listaUsers.appendChild(tr);
         });
